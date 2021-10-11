25e39ed2240f070debe7c5ec05096066c665d818
oicleevan <oicleevan@protonmail.com>
2021-10-11 15:09:12 -0400

    better config integration, new message formatting.

--- src/main/java/me/evan/essentials/Events/JoinLeave.java
+++ src/main/java/me/evan/essentials/Events/JoinLeave.java
@@ -1,25 +1,27 @@
 package me.evan.essentials.Events;
 
+import me.evan.essentials.Essentials;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.plugin.Plugin;
 
 public class JoinLeave implements Listener {
+
+    Plugin plugin = Essentials.getPlugin(Essentials.class);
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
-        e.setJoinMessage(ChatColor.YELLOW + p.getDisplayName() + " has joined the game.");
-
-        System.out.println(p.getDisplayName() + "'s location: " + p.getLocation());
+        e.setJoinMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("joinMessage")) + " " + ChatColor.GRAY + p.getDisplayName());
     }
 
     @EventHandler
     public void onPlayerLeave(PlayerQuitEvent e) {
         Player p = e.getPlayer();
-        e.setQuitMessage(ChatColor.RED + p.getDisplayName() + " has quit the game.");
+        e.setQuitMessage(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("quitMessage")) + " " + ChatColor.GRAY + p.getDisplayName());
     }
 }
---
+++ src/main/java/me/evan/essentials/Events/MessageConfig.java
@@ -0,0 +1,22 @@
+package me.evan.essentials.Events;
+
+import me.evan.essentials.Essentials;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.Plugin;
+
+public class MessageConfig implements Listener {
+
+    Plugin plugin = Essentials.getPlugin(Essentials.class);
+
+    @EventHandler
+    public void onMessage(AsyncPlayerChatEvent e) {
+        Player p = e.getPlayer();
+        String messageFormat = ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("messageFormat").replace("%displayname%", p.getDisplayName()));
+
+        e.setFormat(messageFormat + " " + e.getMessage());
+    }
+}
